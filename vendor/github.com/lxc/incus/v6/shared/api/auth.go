c1705d85be3aa989e770ceb99dad9c9d59049db7
Olav Schwering <osch@defect-track.de>
2026-03-09 22:23:31 +0800

    general cleanup of data
    - remove vendor cache
    - improve .gitignore

--- vendor/github.com/lxc/incus/v6/shared/api/auth.go
+++
@@ -1,9 +0,0 @@
-package api
-
-const (
-	// AuthenticationMethodTLS is the default authentication method for interacting with Incus remotely.
-	AuthenticationMethodTLS = "tls"
-
-	// AuthenticationMethodOIDC is a token based authentication method.
-	AuthenticationMethodOIDC = "oidc"
-)
--- vendor/github.com/lxc/incus/v6/shared/util/filesystem_windows.go
+++
@@ -1,7 +0,0 @@
-//go:build windows
-
-package util
-
-func PathIsWritable(path string) bool {
-	return true
-}
